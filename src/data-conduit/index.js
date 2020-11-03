da1c7c51153087a951fa9d49cc065d6d357f486f
Viking Samurai <vikingsamurai@Vikings-iMac.local>
2020-11-03 14:10:41 -0800

    added new flow for error condition

--- src/data-conduit/index.js
+++ src/data-conduit/index.js
@@ -1,5 +1,16 @@
 'use strict'
 
+/**
+ * Solves equations of the form a * x = b
+ * @example
+ * // returns null
+ * globalNS.method1(5, 10);
+ * @example
+ * // returns 3
+ * globalNS.method(5, 15);
+ * @returns {Number} Returns the value of x for the equation.
+ */
+
 const mongoClient     = require('mongodb').MongoClient
 
 const DataConduit = function() {
@@ -11,10 +22,11 @@ const DataConduit = function() {
   const connect = async () => {
     connection = await mongoClient.connect(connectionString, { useUnifiedTopology: true }).catch(err=>{
       if(err) {
-        console.log({result:'error', message:'database connection error', data: err, connectionData: dbUrl})
+        return {result:'error', message:'database connection error', data: err, connectionData: dbUrl}
       }
-    }) //, function(err, db) {}
+    })
     dbo = connection.db(selectedDatabase)
+    return {status:'ok', message: `connected`}
   }
 
   this.init = async (options={}) => {
@@ -25,9 +37,8 @@ const DataConduit = function() {
         return {status:`error`, message: `connection string parameters not supplied`}
     }
 
-    await connect()
-
-    return dbo ? {status:'ok', message: `connected`} : {status:'error', message: `could not establish connection`}
+    const connectionResult = await connect()
+    return connectionResult
   }
   
   this.connectionStatus = () => {
